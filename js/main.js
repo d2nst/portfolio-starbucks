099913a3cfcb5a4daa7070dfa13b069b36657bb0
d2nst <wlsdl950@gmail.com>
2022-09-28 23:07:52 +0900

    홈

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//
 const searchEl = document.querySelector('.search');
 const serachInputEl = searchEl.querySelector('input');
 
@@ -14,3 +15,26 @@ serachInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   serachInputEl.setAttribute('placeholder', '');
 });
+
+// 스크롤 시 배지 화면에 보여졌다가 사라지게
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      //  gsap.to.(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수,시간)
